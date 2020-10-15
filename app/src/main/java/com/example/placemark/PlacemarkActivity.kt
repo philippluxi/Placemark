b52de445285b406d5b8f8915f3b05db66236124a
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2020-10-15 16:49:56 +0200

    Lab 00 Finished

--- app/src/main/java/com/example/placemark/PlacemarkActivity.kt
+++ app/src/main/java/com/example/placemark/PlacemarkActivity.kt
@@ -2,10 +2,13 @@ package com.example.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class PlacemarkActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    info("Placemark Activity started...")
     setContentView(R.layout.activity_placemark)
   }
 }
