c2c1be75ebda345d4d8cbf70ea78f4fe398ce8cb
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2020-10-24 17:08:21 +0200

    added button event handling

--- app/src/main/java/com/example/placemark/PlacemarkActivity.kt
+++ app/src/main/java/com/example/placemark/PlacemarkActivity.kt
@@ -2,13 +2,24 @@ package com.example.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     info("Placemark Activity started...")
     setContentView(R.layout.activity_placemark)
+
+    btnAdd.setOnClickListener {
+      val placemarkTitle = placemarkTitle.text.toString()
+      if (placemarkTitle.isNotEmpty()) {
+        info("add Button Pressed: $placemarkTitle")
+      } else {
+        toast("Please Enter a title")
+      }
+    }
   }
 }
