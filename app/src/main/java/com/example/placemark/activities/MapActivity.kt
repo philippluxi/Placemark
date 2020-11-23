d890009bd3e7a75a102f7f15823fd9a5893dfab2
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2020-11-23 01:49:22 +0100

    Lab 5 without excercises

--- app/src/main/java/com/example/placemark/activities/MapActivity.kt
+++ app/src/main/java/com/example/placemark/activities/MapActivity.kt
@@ -1,18 +1,21 @@
 package com.example.placemark.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
 import com.example.placemark.R
 import com.example.placemark.models.Location
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     private lateinit var map: GoogleMap
     var location = Location()
@@ -36,5 +39,26 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             .position(loc)
         map.addMarker(options)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+        map.setOnMarkerDragListener(this)
+    }
+
+    override fun onMarkerDragStart(marker: Marker) {
+    }
+
+    override fun onMarkerDrag(marker: Marker) {
+    }
+
+    override fun onMarkerDragEnd(marker: Marker) {
+        location.lat = marker.position.latitude
+        location.lng = marker.position.longitude
+        location.zoom = map.cameraPosition.zoom
+    }
+
+    override fun onBackPressed() {
+        val resultsIntent = Intent()
+        resultsIntent.putExtra("location", location)
+        setResult(Activity.RESULT_OK, resultsIntent)
+        finish()
+        super.onBackPressed()
     }
 }
