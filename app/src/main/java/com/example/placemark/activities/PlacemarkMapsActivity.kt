136492255e911ca66ed0148e02f2b6156a625529
Lup31519 <philipp.luxi@st.oth-regensburg.de>
2020-11-26 02:31:53 +0100

    [Lab 07] Task 04 done - Map Layout filled, map displayed

--- app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/com/example/placemark/activities/PlacemarkMapsActivity.kt
@@ -2,15 +2,46 @@ package com.example.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.activity_placemark_list.*
 import com.example.placemark.R
+import kotlinx.android.synthetic.main.activity_placemark_list.toolbar
+import kotlinx.android.synthetic.main.activity_placemark_maps.*
 
 class PlacemarkMapsActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark_maps)
-
-        setSupportActionBar(toolbar)
         toolbar.title = title
+        setSupportActionBar(toolbar)
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
